04df5a3e9a96b82229bcc3ce22e5060b9fc6d44c
joy <joy@gmail.com>
2019-08-19 17:54:53 +0530

    maven_files

---
+++ src/main/groovy/Web_din/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package Web_din
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
